e677929211835e9f8cf9a0632c571ef98edc3b66
Egor Ivanov <cheshi.mantu@pm.me>
2022-09-27 13:54:00 +0300

    dedv will fail more often

--- tests/utils.ts
+++ tests/utils.ts
@@ -12,15 +12,15 @@ export const attachMicroservice = (val: string) => {
 const isTimeToThrow = () => {
   var failing: number = Math.random();
 
-  if (failing > 0.91 && failing <= 0.92) {
+  if (failing > 0.70 && failing <= 0.80) {
     throw new Error("net::ERR_CONNECTION_REFUSED");
   }
-  if (failing >= 0.93 && failing <= 0.95) {
+  if (failing >= 0.81 && failing <= 0.90) {
     throw new Error(
       "Element not found {selector: something}\n Expected: visible or transparent: visible or have css value opacity=0\n Timeout: 6000 ms"
     );
   }
-  if (failing >= 0.94 && failing <= 0.99) {
+  if (failing >= 0.91 && failing <= 0.99) {
     throw new Error("Test timeout of 30000ms exceeded.");
   }
 };
